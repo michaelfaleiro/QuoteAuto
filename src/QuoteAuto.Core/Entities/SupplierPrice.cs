e9d78a7d5696d2cbc391ed642c17a6636b9057a5
Michael Faleiro <michaelfaleiro@gmail.com>
2025-09-28 19:18:43 -0300

    Incluido campo sku na entidade supplierprice e ajustado request, response e services do supplierprice

--- src/QuoteAuto.Core/Entities/SupplierPrice.cs
+++ src/QuoteAuto.Core/Entities/SupplierPrice.cs
@@ -1,20 +1,33 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace QuoteAuto.Core.Entities;
 
+[BsonIgnoreExtraElements]
 public class SupplierPrice : BaseEntity
 {
     public string SupplierName { get; private set; }
     public decimal Price { get; private set; }
     public string Brand { get; private set; }
+    public string Sku { get; private set; }
     public bool IsSelected { get; private set; }
-
-    public SupplierPrice(string supplierName, decimal price, string brand)
+    
+    public SupplierPrice(string supplierName, decimal price, string brand, string sku)
     {
         SupplierName = supplierName;
         Price = price;
         Brand = brand;
+        Sku = sku;
         IsSelected = false;
     }
 
+    public void UpdateSupplierPrice(string supplierName, decimal price, string brand, string sku)
+    {
+        SupplierName = supplierName;
+        Price = price;
+        Brand = brand;
+        Sku = sku;
+    }
+    
     public void Select()
     {
         IsSelected = true;
